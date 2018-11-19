66cbc5052567bb0a5dcdb651bda6fb8ed1acf878
shub50 <shub50@me.com>
2018-11-19 09:27:28 -0500

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,30 +1,24 @@
-def fvalue(x,f):
-    return eval(f)
-    
-def RAM(a, b, n):
-    h = (b - a) / n
-    x = a
-    i = 1
-    LRAM = 0.0
-    RRAM = 0.0
-    MRAM = 0.0
-    for i in range(n):
-        if x == b:
-            print ('i = ', i)
-        else:
-            LRAM = LRAM + fvalue(x,f)*h
-            x1 = a + (i+1)*h
-            RRAM = RRAM + fvalue(x1,f)*h
-            MRAM = MRAM + fvalue((x + x1)/2,f)*h
-            x = x1
-    return 'Left RAM = {} Right RAM = {} MRAM = {}.'.format(LRAM,RRAM,MRAM)
+a = int(input('Left end point: '))
+b = int(input('Right endpoint: '))
+n = int(input('Number of subintervals: '))
+f = input('Enter function: ')   
 
-def main():
-     a = int(input('Left end point: '))
-     b = int(input('Right endpoint: '))
-     n = int(input('Number of subintervals: '))
-     f = input('Enter function: ')   
-     print (RAM(a, b, n))
-    
-main() 
-        
+h = (b - a) / n
+x = a
+i = 1
+LRAM = 0.0
+RRAM = 0.0
+MRAM = 0.0
+for i in range(n):
+    if x == b:
+        print ('i = ', i)
+    else:
+        LRAM = LRAM + eval(f)*h
+        x1 = x
+        x2 = a + (i+1)*h
+        x = x2
+        RRAM = RRAM + eval(f)*h
+        x = 0.5*(x1 + x2)
+        MRAM = MRAM + eval(f)*h
+        x = x2
+print('Left RAM = {} Right RAM = {} MRAM = {}.'.format(LRAM,RRAM,MRAM))
